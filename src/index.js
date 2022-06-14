bd9ba2d15676030b0ae53c2d31d74d0b9cdb9d8f
Le Minh Hoang <lmhoang0103@gmail.com>
2022-06-14 19:38:59 +0700

    MVC

--- src/index.js
+++ src/index.js
@@ -6,9 +6,16 @@ const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+//Go~ thu muc route -> tu dong nap file index.js trong route vao`
+const route = require("./routes");
+
 //File static
 app.use(express.static(path.join(__dirname, "public")));
 //console.log("Path:  " + path.join(__dirname, "public"));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
 
 //HTTP logger
 app.use(morgan("combined"));
@@ -20,17 +27,12 @@ app.engine(
     extname: ".hbs",
   })
 );
+
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resource/views"));
 
-//route
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+//route init
+route(app);
 
 //127.0.0.1 - localhost
 app.listen(port, () => {
