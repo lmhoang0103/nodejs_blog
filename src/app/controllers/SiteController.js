5239f86c70f34aeee3649a2aa988b9d6ee5026e2
Le Minh Hoang <lmhoang0103@gmail.com>
2022-06-14 21:03:53 +0700

    add model

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,9 +1,18 @@
 const res = require("express/lib/response");
+const Course = require('../models/Course');
 
 class SiteController {
   //[GET] /
   index(req, res) {
-    res.render("home");
+    Course.find({}, function(err, courses){
+      if(!err) 
+      {res.json(courses)} else {
+
+        res.status(400).json({error: 'ERROR!!!'})
+      }
+
+    })
+    //res.render("home");
   }
   //[GET] /search
   search(req, res) {
--- src/index.js
+++ src/index.js
@@ -3,22 +3,26 @@ const morgan = require("morgan");
 const path = require("path");
 const handlebars = require("express-handlebars");
 
-const app = express();
-const port = 3000;
-
+const db = require("./config/db");
 //Go~ thu muc route -> tu dong nap file index.js trong route vao`
 const route = require("./routes");
 
+//Connect DB
+db.connect();
+
+const app = express();
+const port = 3000;
+
 //File static
-app.use(express.static(path.join(__dirname, "public")));
+app.get(express.static(path.join(__dirname, "public")));
 //console.log("Path:  " + path.join(__dirname, "public"));
 
-app.use(express.urlencoded({ extended: true }));
+app.get(express.urlencoded({ extended: true }));
 
-app.use(express.json());
+app.get(express.json());
 
 //HTTP logger
-app.use(morgan("combined"));
+app.get(morgan("combined"));
 
 //Template engine
 app.engine(
@@ -29,12 +33,12 @@ app.engine(
 );
 
 app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resource/views"));
+app.set("views", path.join(__dirname, "resource", "views"));
 
 //route init
 route(app);
 
 //127.0.0.1 - localhost
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
